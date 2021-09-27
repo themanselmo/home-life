c4f3bf9c5f6acdf9618978ffb59f94f616a682ba
max anselmo <itmanselmo@gmail.com>
2021-09-26 22:56:28 -0400

    finished init functions for each plany type, now need to finish updates mentioned at the top of index.js, also updated the css so each plant card has a unique color

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
+/*
+        Need to make it so each time a link button is clicked, the
+    plant div is cleared so that old contents are removed, and 
+    to limit the times each button can be cleared to once so 
+    you can't add the same plants multiple times by clicking 
+    the same link
+*/
+
 const plants = [
     {
         id: '1',
@@ -7,85 +15,72 @@ const plants = [
         oftenToWater: 'every 14 days',
         amtOfSun: 'partial sun',
     },
-    // {
-    //     id: '',
-    //     name: '',
-    //     typeOfPlant: '',
-    //     img: '',
-    //     oftenToWater: '',
-    //     amtOfSun: '',
-    // },
-    // {
-    //     id: '',
-    //     name: '',
-    //     typeOfPlant: '',
-    //     img: '',
-    //     oftenToWater: '',
-    //     amtOfSun: '',
-    // },
-    // {
-    //     id: '',
-    //     name: '',
-    //     typeOfPlant: '',
-    //     img: '',
-    //     oftenToWater: '',
-    //     amtOfSun: '',
-    // },
-    // {
-    //     id: '',
-    //     name: '',
-    //     typeOfPlant: '',
-    //     img: '',
-    //     oftenToWater: '',
-    //     amtOfSun: '',
-    // },
-    // {
-    //     id: '',
-    //     name: '',
-    //     typeOfPlant: '',
-    //     img: '',
-    //     oftenToWater: '',
-    //     amtOfSun: '',
-    // },
-    // {
-    //     id: '',
-    //     name: '',
-    //     typeOfPlant: '',
-    //     img: '',
-    //     oftenToWater: '',
-    //     amtOfSun: '',
-    // },
-    // {
-    //     id: '',
-    //     name: '',
-    //     typeOfPlant: '',
-    //     img: '',
-    //     oftenToWater: '',
-    //     amtOfSun: '',
-    // },
-    // {
-    //     id: '',
-    //     name: '',
-    //     typeOfPlant: '',
-    //     img: '',
-    //     oftenToWater: '',
-    //     amtOfSun: '',
-    // },
-    // {
-    //     id: '',
-    //     name: '',
-    //     typeOfPlant: '',
-    //     img: '',
-    //     oftenToWater: '',
-    //     amtOfSun: '',
-    // }
+    {
+        id: '2',
+        name: 'Jade Plant',
+        typeOfPlant: 'Succulent',
+        // img: '',
+        oftenToWater: 'every 10 to 14 days',
+        amtOfSun: 'partial sun',
+    },
+    {
+        id: '3',
+        name: 'Xanadu',
+        typeOfPlant: 'Philodendron',
+        // img: '',
+        oftenToWater: 'every 7-14 days',
+        amtOfSun: 'partial sun',
+    },
+    {
+        id: '4',
+        name: 'Heartleaf',
+        typeOfPlant: 'Philodendron',
+        // img: '',
+        oftenToWater: 'every 7-14 days',
+        amtOfSun: 'partial sun',
+    },
+    {
+        id: '5',
+        name: '',
+        typeOfPlant: 'Woodie',
+        img: '',
+        oftenToWater: '',
+        amtOfSun: '',
+    },
+    {
+        id: '6',
+        name: '',
+        typeOfPlant: 'Woodie',
+        img: '',
+        oftenToWater: '',
+        amtOfSun: '',
+    },
+    {
+        id: '7',
+        name: '',
+        typeOfPlant: 'Annual',
+        img: '',
+        oftenToWater: '',
+        amtOfSun: '',
+    },
+    {
+        id: '8',
+        name: '',
+        typeOfPlant: 'Annual',
+        img: '',
+        oftenToWater: '',
+        amtOfSun: '',
+    }
 ];
 
 const plantBox = document.getElementById('content-container');
 
+//  the renderPlant function builds each plant card to be displayed,
+// drawing each piece of data from the plant item being iterated through
+// when the init function is called
 function renderPlant(plant) {
     let plantCard = document.createElement('div');
-    plantCard.className = 'plant-card';
+    plantCard.className = `${plant.typeOfPlant}-Card`;
     
     // let plantImg = document.createElement('img');
     // plantImg.src = plant.img;
@@ -107,8 +102,53 @@ function renderPlant(plant) {
     plantBox.append(plantCard);
 }
 
+//  each event listener here gets tied to the appropriate link / button
+// to be activated upon the click event
+let succulentLink = document.getElementById('succulent-link');
+succulentLink.addEventListener('click', initSucculents);
+
+let philoLink = document.getElementById('philo-link');
+philoLink.addEventListener('click', initPhilodendron);
+
+let woodieLink = document.getElementById('woodie-link');
+woodieLink.addEventListener('click', initWoodie);
+
+let annualLink = document.getElementById('annual-link');
+annualLink.addEventListener('click', initAnnual);
+
+let viewAllLink = document.getElementById('viewAll-link');
+viewAllLink.addEventListener('click', initAll);
+
+//  init function renders the plant contents to the page after 
+// the appropriate event was fired off
 function initAll() {
     plants.forEach(renderPlant);
 }
 
-initAll();
+function initSucculents() {
+    let succulentList = plants.filter((plant) => {
+        return plant.typeOfPlant === 'Succulent';
+    })
+    succulentList.forEach(renderPlant);
+};
+
+function initPhilodendron() {
+    let philoList = plants.filter((plant) => {
+        return plant.typeOfPlant === 'Philodendron';
+    });
+    philoList.forEach(renderPlant);
+}
+
+function initWoodie() {
+    let woodieList = plants.filter((plant) => {
+        return plant.typeOfPlant === 'Woodie';
+    });
+    woodieList.forEach(renderPlant);
+}
+
+function initAnnual() {
+    let annualList = plants.filter((plant) => {
+        return plant.typeOfPlant === 'Annual';
+    });
+    annualList.forEach(renderPlant);
+}
